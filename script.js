9c0bec0b1a981b23f253d656f970d01868e1d9ff
mllim15 <michelle.lim15@gmail.com>
2021-02-06 17:02:22 -0800

    added function for reset

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 $(document).ready(function () {
     // 9 box puzzle
     let rows = 3, columns = 3;
-    let puzzlePieces = "";
+    startingPuzzle();
 
-    /* loop to create divs for the puzzle pieces
-       and change top and left positions of the image
-       images are 540px x 540px, 3 puzzle pieces in each row = 540/3 */
-    for (let i = 0, topPosition = 0; i < rows; i++, topPosition -= 180) {
-        for (let j = 0, leftPosition = 0; j < columns; j++, leftPosition -= 180) {
-            puzzlePieces += "<div class='piece' style='background-position:" + leftPosition + "px " + topPosition + "px;'></div>";
-        }
-    }
-    $(".imgContainer").html(puzzlePieces);
-
-    /* actions when solve button is clicked */
     $("#solve").click(function () {
+        solveClicked();
+    });
+
+    // actions when reset button is clicked
+    $("#reset").click(function () {
+        resetClicked();
+    });
+
+    // function for the start of the puzzle
+    function startingPuzzle() {
+        let puzzlePieces = "";
+
+        /* loop to create divs for the puzzle pieces
+           and change top and left positions of the image
+           images are 540px x 540px, 3 puzzle pieces in each row = 540/3 */
+        for (let i = 0, topPosition = 0; i < rows; i++, topPosition -= 180) {
+            for (let j = 0, leftPosition = 0; j < columns; j++, leftPosition -= 180) {
+                puzzlePieces += "<div class='piece' style='background-position:" + leftPosition + "px " + topPosition + "px;'></div>";
+            }
+        }
+        $(".imgContainer").html(puzzlePieces);
+    }
+
+    // when solve is clicked do this
+    function solveClicked() {
         let puzzlePieces = $(".imgContainer div");
         // randomize the left and top positions of the puzzle pieces
         puzzlePieces.each(function () {
@@ -40,20 +54,29 @@ $(document).ready(function () {
         }
         $(".imgContainer").html(emptyPuzzlePieces);
         // hide the solve button
-        $(this).attr("hidden", true);
+        $("#solve").attr("hidden", true);
         // show the reset button
         $("#reset").attr("hidden", false);
 
         draggableAndDroppableOn();
-    });
+    }
 
-    /* actions when reset button is clicked */
-    $("#reset").click(function () {
+    // when reset is clicked do this
+    function resetClicked() {
         // hide the reset button
-        $(this).attr("hidden", true);
+        $("#reset").attr("hidden", true);
+        // show the solve button
         $("#solve").attr("hidden", false);
-    });
 
+        // delete the divs of the puzzle pieces from both containers
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                $(".draggable").remove();
+                $(".droppable").remove();
+            }
+        }
+        startingPuzzle();
+    }
 
     // make randomized puzzle pieces draggable
     function draggableAndDroppableOn() {
@@ -66,10 +89,10 @@ $(document).ready(function () {
 
                 droppableSpace.addClass("full");
                 $(draggablePiece).addClass("dropped").css({
-                    left: 0, top: 0, position: "relative"
+                    left: 0, top: 0, position: "relative", border: "none"
                 }).appendTo(droppableSpace);
             }
         });
     }
-
 })
+
